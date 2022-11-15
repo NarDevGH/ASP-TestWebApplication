15f0505f38f4967038c6f3690709aaeb0f2727a7
NarMaroth <bautista.mateo@outlook.com>
2022-11-15 01:05:34 -0300

    first commit

---
+++ WebApplication1_ASPCore_MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1_ASPCore_MVC.Models;
+
+namespace WebApplication1_ASPCore_MVC.Controllers
+{
+    // Cuando se devuelve solo view sin indicar la vista el framework la encuentra fijandose el nombre del controlador,
+    // busca la carpeta en vistas que tenga ese nombre, y luego busca el la vista que se llame igual que el metodo 
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> s_dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(s_dogs);
+        }
+
+        public string HelloWorld() 
+        {
+            return "Hello World";
+        }
+
+        public IActionResult Create() 
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+            
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel) 
+        {
+            //return View("Index");
+            s_dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebApplication1_ASPCore_MVC/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1_ASPCore_MVC.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
